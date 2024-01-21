6a83f0b00f89cbb18481fe2da19693d675f6c4e8
svetoslav.hristov <svetoslav.hristov2001@gmail.com>
2024-01-21 23:25:32 +0100

    dependency check added

--- src/main/java/com/fightfoodwaste/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/fightfoodwaste/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.discoveryserver;
+package com.fightfoodwaste.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
